231ed7deeeba059af96855188850ddd8903c3dc4
Pritam Dash <pritamdash2015@outlook.com>
2022-06-05 13:25:26 +0530

    Completed the 1st app to add 2 numbers and display it on the screen.

--- MyFirst-ios-app/MyFirst-ios-app/ViewController.swift
+++ MyFirst-ios-app/MyFirst-ios-app/ViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //These are all references of the objects that we drag and droppe din the Screen.
+    //So var label1 : UILabel = UILabel() is not required.
+    //To make the connection of these references to the objects created on the UI screen we put @IBOutlet while declaring.
+    @IBOutlet var label1 : UILabel?
+    @IBOutlet var label2 : UILabel?
+    @IBOutlet var label3 : UILabel?
+    
+    @IBOutlet var field1 : UITextField?
+    @IBOutlet var field2 : UITextField?
+    
+    @IBOutlet var button1 : UIButton?
+    
 
     override func viewDidLoad() {
+        //This viewDidLoad() is similar to main() in different language, so we don't need to call it separately.
+        //This function is the first function to run.
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label3?.numberOfLines = 0
+    }
+    
+    @IBAction func sum() -> Void {
+        var x : Int = 7
+        var y : Int = 9
+        var z : Int = 0
+        
+        x = Int(field1!.text!)!
+        y = Int(field2!.text!)!
+        z = x+y
+        
+        label3?.text = "Result = \(z)"
     }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FirstPersonal-iOS-App
- The first ios app on my personal macbook air
+ The first ios app on my personal macbook air m1
+ Attaching a screen recording of the app.
+/Users/pritamdash/Desktop/Simulator Screen Recording - iPhone 12 - 2022-06-05 at 13.15.58.mp4
+Adding of 2 numbers and showing the output.
+Used : Xcode, UIKit, Storyboard and Swift 5
